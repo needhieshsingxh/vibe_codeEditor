1d810e88665d44563acb6ff98794d93924f249d8
Needhiesh Singh <needhieshsingxh@users.noreply.github.com>
2026-03-18 23:31:35 +0530

    feat: implement template file tree and node components
    - Added TemplateFileTree component for displaying a hierarchical file structure with options to add, delete, and rename files and folders.
    - Introduced TemplateNode component to represent individual files and folders within the tree.
    - Created NewFileDialog and NewFolderDialog components for user input when creating new files and folders.
    - Implemented useFileExplorer hook to manage file state, including opening, closing, adding, and deleting files and folders.
    - Added utility functions for file path generation and ID creation.

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -3,12 +3,38 @@ import React from 'react'
 import {useParams} from "next/navigation";
 import { SidebarTrigger, SidebarInset } from '@/components/ui/sidebar';
 import {Separator} from "@/components/ui/separator"
+import TemplateFileTree from '@/modules/playground/componets/template-file-tree';
+import{usePlayground} from "@/modules/playground/hooks/usePlayground"
+
 
 const page = () => {
     const{id} = useParams<({id:string})>();
+    const {playgroundData, templateData, isLoading, error, saveTemplateData} = usePlayground(id);
+      const {
+    activeFileId,
+    closeAllFiles,
+    openFile,
+    closeFile,
+    editorContent,
+    updateFileContent,
+    handleAddFile,
+    handleAddFolder,
+    handleDeleteFile,
+    handleDeleteFolder,
+    handleRenameFile,
+    handleRenameFolder,
+    openFiles,
+    setTemplateData,
+    setActiveFileId,
+    setPlaygroundId,
+    setOpenFiles,
+  } = useFileExplorer();
+
+  
   return (
   <div>
     <>
+    < TemplateFileTree data={templateData} />
         <SidebarInset>
             <header className="flex h-16 shrink=0 items-center gap-2 border-b px-4">
                 <SidebarTrigger className="-ml-1"/>
