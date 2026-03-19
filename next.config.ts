ce9c50a269e67ac00c3784435fcd061b185754dd
Needhiesh Singh <needhieshsingxh@users.noreply.github.com>
2026-03-19 16:04:49 +0530

    feat(webcontainer): implement WebContainer integration with terminal and file management

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,32 @@ const nextConfig: NextConfig = {
   images:{
     remotePatterns:[
       {
-        protocol: "https",
-        hostname: "*",
-        port: "",
+        protocol:"https",
+        hostname:"*",
+        port:'',
         pathname:"/**"
       }
     ]
-  }
+  },
+  async headers() {
+    return [
+      {
+        // Apply to all routes
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ];
+  },
+  reactStrictMode:false
 };
 
 export default nextConfig;
