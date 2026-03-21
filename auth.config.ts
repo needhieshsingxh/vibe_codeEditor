f513058ba96e4c0b40cb2763501fca0a05de2370
Needhiesh Singh <needhieshsingxh@users.noreply.github.com>
2026-03-21 17:50:45 +0530

    fix(auth): enable oauth email account linking for existing users

--- auth.config.ts
+++ auth.config.ts
@@ -9,10 +9,12 @@ export default {
     Github({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     Google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
 } satisfies NextAuthConfig;
